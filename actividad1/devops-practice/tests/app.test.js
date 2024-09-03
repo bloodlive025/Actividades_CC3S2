d933f757c3728048cb30906f5cd5d204bcec58ea
Omar <darknoon123@example.com>
2024-09-03 15:30:04 -0500

    ImplementarTiempoDeEspera

--- actividad1/devops-practice/tests/app.test.js
+++ actividad1/devops-practice/tests/app.test.js
@@ -1,6 +1,10 @@
 const request = require('supertest');//Importa la biblioteca supertest para simular solicitudes HTTPS
 const app = require('../src/app');//Importa la aplicacion Express app.js, lo que permitira realizar pruebas sobre la aplicacion
 describe('GET /', () => {
+	beforeEach(() => {
+	  jest.setTimeout(10000); // Establece el tiempo de espera a 10 segundos
+		  });
+
 	it('should return Hello, World!', async () => {
 	const res = await request(app).get('/');
 	expect(res.statusCode).toEqual(200);
