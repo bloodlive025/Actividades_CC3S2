def8b0b8562b7cef802f14a01e6aaf3e6b18d864
Omar <darknoon123@example.com>
2024-09-03 15:54:23 -0500

    Actualizarapp.test.js

--- actividad1/devops-practice/tests/app.test.js
+++ actividad1/devops-practice/tests/app.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');//Importa la biblioteca supertest para simular solicitudes HTTPS
 const app = require('../src/app');//Importa la aplicacion Express app.js, lo que permitira realizar pruebas sobre la aplicacion
 
+let server;
+
 beforeAll((done) => {
     server = app.listen(() => {
         console.log(`Server is running on port ${server.address().port}`);
